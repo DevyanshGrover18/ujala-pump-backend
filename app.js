6606c727bbcc07e3f427f57444ab5c6ee9ae7ae8
Devyansh Grover <devyansh.grover348@gmail.com>
2026-07-03 16:29:01 +0530

    CORS

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ app.use(
       'https://www.ujalapump.com',
       'http://192.168.31.156:5173',
       'https://ujala-latest-development.vercel.app',
+      'https://ujalapump.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
